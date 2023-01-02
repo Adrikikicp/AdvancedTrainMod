b7fed06785e2d94a4c58921249f49c2d0256e8a9
Adrikikicp <adrikikicp@gmail.com>
2023-01-02 14:56:42 -0500

    Add mixin configuration file and mcmod.info file

---
+++ src/main/java/com/adrikikicp/atm/ATM.java
@@ -0,0 +1,14 @@
+package com.adrikikicp.atm;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.relauncher.FMLLaunchHandler;
+import org.spongepowered.asm.launch.MixinBootstrap;
+
+@Mod(modid="atm")
+public class ATM {
+
+public ATM(){
+
+}
+
+}
